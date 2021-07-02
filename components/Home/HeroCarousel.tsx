ab1127d4e6d26cbbe29adc7a08c045f3106b7463
Gunjan Bangde <gunjan.bangde@theinternetfolks.com>
2021-07-02 13:39:59 +0530

    build issue fixes

--- components/Home/HeroCarousel.tsx
+++ components/Home/HeroCarousel.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import { Flex, Img, Box, Text } from "@chakra-ui/react";
+import { Flex, Img, Box } from "@chakra-ui/react";
 
 type Props = {
   // setIndex: React.Dispatch<React.SetStateAction<number>>;
@@ -10,33 +10,6 @@ type Props = {
 };
 
 const ThemeCarousel: React.FC<Props> = () => {
-  const carouselData = [
-    {
-      text: "Agricultural",
-      icon: "agriculture",
-    },
-    {
-      text: "Public Transportation OR Public Safety",
-      icon: "transportation",
-    },
-    {
-      text: "Home Automation & Application",
-      icon: "smart_system",
-    },
-    {
-      text: "Renewable Energy",
-      icon: "renewable",
-    },
-    {
-      text: "Waste Management",
-      icon: "waste",
-    },
-    {
-      text: "Open Innovation",
-      icon: "open",
-    },
-  ];
-
   return (
     <Box mt="6rem" height="70%" width="100%">
       <Carousel
@@ -47,7 +20,7 @@ const ThemeCarousel: React.FC<Props> = () => {
         showArrows={true}
       >
         {[...Array(10)].map((item, i) => (
-          <Flex px="1rem" height="100%" alignItems="center">
+          <Flex key={i} item={item} px="1rem" height="100%" alignItems="center">
             <Img
               maxHeight="500px"
               maxWidth="100%"
